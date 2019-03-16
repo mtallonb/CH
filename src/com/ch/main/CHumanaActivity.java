4f8df1d5fa17abe5df0d2bbdd1773aa20737cb55
Miguel <mtallonb@gmail.com>
2019-03-16 13:04:21 +0100

    Initial commit

---
+++ src/com/ch/main/CHumanaActivity.java
@@ -0,0 +1,13 @@
+package com.ch.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CHumanaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
